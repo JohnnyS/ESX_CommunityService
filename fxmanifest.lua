e87d86d184eef99b27deb1f9911e84654167522c
JohnnyS <62607662+JohnnyS@users.noreply.github.com>
2022-05-15 00:07:44 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,8 +2,10 @@ fx_version 'adamant'
 game 'gta5'
 
 description 'ESX Community Service'
-
+lua54 'yes'
 version '1.1.1'
+
+shared_script {'@es_extended/imports.lua', '@ox_lib/init.lua'}
 
 server_scripts {
 	'@mysql-async/lib/MySQL.lua',
@@ -25,3 +27,4 @@ client_scripts {
 }
 
 dependency 'es_extended'
+
